769e46e30cb185f4584b5067741af4b997f0f4c2
nakshayan <nakshayanplays@outlook.com>
2024-07-16 17:19:50 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,60 +1,56 @@
-import { getBlockedWebsites } from "./storageManager.js";
-import { isWebsiteBlocked } from "./utils.js";
-import { loadBlockedWebsites } from "./storageManager.js";
+import { getBlockedWebsites } from './storageManager.js';
+import { isWebsiteBlocked } from './utils.js';
+import { loadBlockedWebsites } from './storageManager.js';
 
 export let websitesLoadedPromise = loadBlockedWebsites();
 
 export const updateWebsitesLoadedPromise = () => {
-  websitesLoadedPromise = loadBlockedWebsites();
+    websitesLoadedPromise = loadBlockedWebsites();
 };
 
 export const awaitWebsitesLoaded = async () => {
-  try {
-    await websitesLoadedPromise;
-  } catch (error) {
-    console.error(`[15s] Error loading websites: ${error}`);
-  }
+    try {
+        await websitesLoadedPromise;
+    } catch (error) {
+        console.error(`[15s] Error loading websites: ${error}`);
+    }
 };
 
-let previousWebsite = "";
+let previousWebsite = '';
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status !== "complete") {
-    return;
-  }
-
-  if (!tab.url) {
-    return;
-  }
-
-  const urlObj = new URL(tab.url);
-  const domain = urlObj.hostname;
-  const queryParams = new URLSearchParams(urlObj.search);
-  const isInPlaylist = queryParams.has("list");
-
-  if (
-    domain === "www.youtube.com" &&
-    previousWebsite !== urlObj.href &&
-    !isInPlaylist
-  ) {
-    chrome.tabs.reload(tabId, {}, () => {
-      console.log("[15s] Tab reloaded.");
-    });
-  }
-  previousWebsite = urlObj.href;
-
-  awaitWebsitesLoaded();
-  const blockedWebsites = getBlockedWebsites();
-  console.log("[15s] Blocked websites:", blockedWebsites);
-  try {
-    const isBlocked = isWebsiteBlocked(domain, blockedWebsites);
-    if (isBlocked) {
-      console.log("[15s] Tab updated");
-      chrome.tabs.sendMessage(tabId, {
-        message: "BLOCK",
-      });
+    if (changeInfo.status !== 'complete') {
+        return;
     }
-  } catch (error) {
-    console.error(`[15s] Could not block: ${error}`);
-  }
+    
+    if (!tab.url) {
+        return;
+    }
+    
+    const urlObj = new URL(tab.url);
+    const domain = urlObj.hostname;
+    const queryParams = new URLSearchParams(urlObj.search);
+    const isInPlaylist = queryParams.has('list');
+
+    awaitWebsitesLoaded();
+    const blockedWebsites = getBlockedWebsites();
+    console.log('[15s] Blocked websites:', blockedWebsites)
+    try {
+        const isBlocked = isWebsiteBlocked(domain, blockedWebsites);
+        if (domain === 'www.youtube.com' && previousWebsite !== urlObj.href && !isInPlaylist) {
+            chrome.tabs.reload(tabId, {}, () => {
+                console.log('[15s] Tab reloaded.');
+            });
+        }
+        
+        previousWebsite = urlObj.href;
+        if (isBlocked) {
+            console.log('[15s] Tab updated');
+            chrome.tabs.sendMessage(tabId, {
+                message: "BLOCK"
+            });
+        }
+    } catch (error) {
+        console.error(`[15s] Could not block: ${error}`);
+    }
 });
